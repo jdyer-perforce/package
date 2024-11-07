de2dbe0cf30080e5f3ecbfbf3407eba2e40c4a62
jdyer-perforce <jdyer@perforce.com>
2024-11-07 13:47:24 +0000

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,1 +1,3 @@
-res.setHeader("Set-Cookie", "token=" + rand + "; Secure")
+res.setHeader("Set-Cookie", "token=" + token + "; Secure")
+const { createHash } = require('crypto');
+const hash = createHash('sha256').update(token).digest('hex');
